b2f19c7fa0fff286672624a4b2e5264ddff3b98b
Shandeep Singh <shandeepsingh96@gmail.com>
2019-11-04 12:50:58 -0500

    init

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter from './routes/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibileExpenses from './selectors/expenses'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt:1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+
+const state = store.getState();
+const visibileExpenses = getVisibileExpenses(state.expenses, state.filters);
+console.log(visibileExpenses);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
