616a3f33eb894449fcacb281e764c547fd579bc4
Shandeep Singh <shandeepsingh96@gmail.com>
2019-11-04 18:09:32 -0500

    test

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,47 +1,49 @@
-import { setStartDate, setEndDate, setTextFilter, sortByDate, sortByAmount } from '../../actions/filters';
 import moment from 'moment';
+import {
+  setStartDate,
+  setEndDate,
+  setTextFilter,
+  sortByAmount,
+  sortByDate
+} from '../../actions/filters';
 
-test('Should generate set start date object', () => {
-    const action = setStartDate(moment(0));
-    expect(action).toEqual({
-        type: 'SET_START_DATE',
-        startDate: moment(0)
-    });
+test('should generate set start date action object', () => {
+  const action = setStartDate(moment(0));
+  expect(action).toEqual({
+    type: 'SET_START_DATE',
+    startDate: moment(0)
+  });
 });
 
-test('Should generate set end date object', () => {
-    const action = setEndDate(moment(0));
-    expect(action).toEqual({
-        type: 'SET_END_DATE',
-        endDate: moment(0)
-    });
+test('should generate set end date aciton object', () => {
+  const action = setEndDate(moment(0));
+  expect(action).toEqual({
+    type: 'SET_END_DATE',
+    endDate: moment(0)
+  });
 });
 
-test('Should generate set text filter object with text value', () => {
-    const text = 'testing';
-    const action = setTextFilter(text);
-    expect(action).toEqual({
-        type: 'SET_TEXT_FILTER',
-        text
-    });
+test('should generate set text filter object with text value', () => {
+  const text = 'Something in';
+  const action = setTextFilter(text);
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text
+  });
 });
 
-test('Should generate set text filter object with default values', () => {
-    const action = setTextFilter();
-    expect(action).toEqual({
-        type: 'SET_TEXT_FILTER',
-        text: ''
-    });
+test('should generate set text filter object with default', () => {
+  const action = setTextFilter();
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: ''
+  });
 });
 
-test('Should generate sort by date object', () => {
-    expect(sortByDate()).toEqual({
-        type: 'SORT_BY_DATE'
-    });
+test('should generate action object for sort by date', () => {
+  expect(sortByDate()).toEqual({ type: 'SORT_BY_DATE' });
 });
 
-test('Should generate sort by amount object', () => {
-    expect(sortByAmount()).toEqual({
-        type: 'SORT_BY_AMOUNT'
-    });
+test('should generate action object for sort by amount', () => {
+  expect(sortByAmount()).toEqual({ type: 'SORT_BY_AMOUNT' });
 });
