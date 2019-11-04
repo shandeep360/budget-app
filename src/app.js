04adc561f07b3f10b103653ee7f9ea0545dc39bd
Shandeep Singh <shandeepsingh96@gmail.com>
2019-11-04 18:28:54 -0500

    webpack config fixed

--- src/app.js
+++ src/app.js
@@ -10,14 +10,7 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt:1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-const state = store.getState();
-const visibileExpenses = getVisibileExpenses(state.expenses, state.filters);
-console.log(visibileExpenses);
+console.log('test');
 
 const jsx = (
     <Provider store={store}>
