6ed7db074a472a448e3680997b10140f31c01780
Shandeep Singh <shandeepsingh96@gmail.com>
2019-11-05 18:29:03 -0500

    setup prod build

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ import { setTextFilter } from './actions/filters';
 import getVisibileExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-console.log('test');
 
 const jsx = (
     <Provider store={store}>
