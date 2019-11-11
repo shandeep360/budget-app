7312b6631cf94afe101f24a82b110f8a041fe5d4
Shandeep Singh <shandeepsingh96@gmail.com>
2019-11-11 15:14:44 -0500

    added all styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Budgeter</h1>
-      <p>It's time to get your expenses under control.</p>
+      <p>Keep track of where your money goes.</p>
       <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
